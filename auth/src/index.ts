5b43cc834eb7bf948440bbabfd11f0a7400d69ae
Jarod Christiansen <jarodchristiansen2@gmail.com>
2023-02-20 16:35:21 -0700

    mongodb/user service/hashed passwords

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import { json } from "body-parser";
 import "express-async-errors";
+
+import mongoose from "mongoose";
+
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
@@ -22,6 +25,18 @@ app.all("*", async (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000!! !");
-});
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+
+    console.log("CONNECTED TO MONGODB");
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log("Listening on port 3000!! !");
+  });
+};
+
+start();
